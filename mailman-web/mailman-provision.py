97ee89d30e8dd7a19553fbcb06df259d651c88a8
Marc Tardif <marc@taram.ca>
2026-04-16 15:34:14 -0400

    Fix provisioning initial domain

---
+++ mailman-web/mailman-provision.py
@@ -0,0 +1,18 @@
+import os
+from mailmanclient import Client
+
+domain = os.environ.get('SERVE_FROM_DOMAIN', '')
+if not domain:
+    raise SystemExit('SERVE_FROM_DOMAIN is not set')
+
+client = Client(
+    os.environ.get('MAILMAN_REST_API_URL', 'http://mailman-core:8001/3.1/'),
+    os.environ.get('MAILMAN_REST_API_USER', 'restadmin'),
+    os.environ.get('MAILMAN_REST_API_PASS', 'restpass'),
+)
+
+if domain not in [d.mail_host for d in client.domains]:
+    client.create_domain(domain)
+    print('Domain {} created'.format(domain))
+else:
+    print('Domain {} already exists'.format(domain))
--- mailman-web/settings_local.py
+++ mailman-web/settings_local.py
@@ -1,5 +1,7 @@
 DEBUG = False
 
+LANGUAGE_CODE = 'fr-ca'
+
 MAILMAN_WEB_SOCIAL_AUTH = []
 
 DEFAULT_FROM_EMAIL = 'mailman@lists.example.org'
